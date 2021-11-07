122043c9464621ba1df39a1957b5301fcd0279e1
Antonin Carlin <antonin.carlin@icloud.com>
2021-11-07 11:41:06 +0100

    tests

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let posY = [];
+let lastScrollTop = 0;
 const slider = document.querySelector(".slider");
 const input = document.querySelector(".slider__input");
 
@@ -6,27 +7,40 @@ function openSlider() {
   slider.classList.remove("slider--close--really");
   slider.classList.remove("slider--close");
   slider.classList.add("slider--open");
-  input.focus();
+  lastScrollTop = window.scrollY;
+  // iosFocus(input, 300);
 }
 
 function closeSlider() {
   slider.classList.remove("slider--open");
   slider.classList.add("slider--close");
+  window.scrollTo(0, lastScrollTop, "smooth");
   setTimeout(() => {
     slider.classList.add("slider--close--really");
   }, 500);
 }
 
-function getPosY(e) {
-  e.preventDefault();
-  posY.push(e.touches[0].clientY);
-  // if values are negative, set to 0
-  const pos = posY.reduce((a, b) => {
-    return a - b;
-  });
-  if (posY.length < 3) return;
-  if (pos < 0) {
-    closeSlider();
-    posY = [];
-  }
+function iosFocus(targetInput, delay = 0) {
+  // create invisible dummy input to receive the focus first
+  const fakeInput = document.createElement("input");
+  fakeInput.setAttribute("type", "text");
+  fakeInput.style.position = "absolute";
+  fakeInput.style.opacity = 0;
+  fakeInput.style.height = 0;
+  fakeInput.style.fontSize = "16px"; // disable auto zoom
+
+  // you may need to append to another element depending on the browser's auto
+  // zoom/scroll behavior
+  document.body.prepend(fakeInput);
+
+  // focus so that subsequent async focus will work
+  fakeInput.focus();
+
+  setTimeout(() => {
+    // now we can focus on the target input
+    targetInput.focus();
+
+    // cleanup
+    fakeInput.remove();
+  }, delay);
 }
